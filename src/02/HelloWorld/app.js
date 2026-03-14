3012428d6188333e36871859aee74536107d60dd
Pep Lainez <jlainezs@gmail.com>
2026-03-14 07:20:34 +0100

    Add additional middleware

--- src/02/HelloWorld/app.js
+++ src/02/HelloWorld/app.js
@@ -2,8 +2,18 @@ var express = require('express');
 var http = require('http');
 
 var app = express();
-app.use(function(req, res) {
+
+// middleware function to log the request path
+app.use(function(req, res, next) {
     console.log("In comes a request to: " + req.url);
+    next();
+});
+
+// middleware function to send the response
+// it is the last middleware function in the stack, so it doesn't call next()
+app.use(function (request, response) {
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.end("Hello, world!");
 });
 
 http.createServer(app).listen(3000);
